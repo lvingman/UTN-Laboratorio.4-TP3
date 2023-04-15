b7757d02e8db5bae41bf93872564844909100e74
kylaren <86126109+kylaren@users.noreply.github.com>
2023-04-15 10:49:17 -0300

    new class Archivo

---
+++ TP3_GRUPO_7/src/EJERCICIO1/Archivo.java
@@ -0,0 +1,61 @@
+package EJERCICIO1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Archivo {
+	
+	private String ruta;
+
+	// constructores
+	public Archivo() {
+		
+	}
+	
+	public Archivo(String ruta) {
+		this.ruta = ruta;
+	}
+	
+	// gets & sets
+	public String getRuta() {
+		return ruta;
+	}
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+
+	// compruebo si existe el archivo
+	public boolean existe()
+	{
+		File archivo = new File(ruta); // ruta puede recibir una ruta relativa o absoluta
+		if(archivo.exists())
+			return true;
+		return false;
+	}
+	
+	// lee el archivo
+	public void lee_lineas() {
+		FileReader entrada;
+		try {
+			entrada = new FileReader(ruta);
+			BufferedReader miBuffer = new BufferedReader(entrada);
+			
+			String linea = "";
+				while (linea != null) {
+					System.out.println(linea);
+					linea = miBuffer.readLine();
+				} 
+			
+			miBuffer.close();
+			entrada.close();
+			
+		}catch(IOException e) {
+			System.out.println("No se encontro el archivo");
+		}
+		
+	}
+
+}
