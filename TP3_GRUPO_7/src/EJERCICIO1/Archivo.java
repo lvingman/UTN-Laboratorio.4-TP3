05239d902894974eff94015d5a4dcdcd1fcac331
kylaren <86126109+kylaren@users.noreply.github.com>
2023-04-20 09:55:38 -0300

    punto C

--- TP3_GRUPO_7/src/EJERCICIO1/Archivo.java
+++ TP3_GRUPO_7/src/EJERCICIO1/Archivo.java
@@ -1,8 +1,10 @@
 package EJERCICIO1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.TreeSet;
 
@@ -26,6 +28,21 @@ public class Archivo {
 
 	public void setRuta(String ruta) {
 		this.ruta = ruta;
+	}
+	
+	// crea un nuevo archivo
+	public boolean crearArchivo()
+	{
+		FileWriter escritura;
+		try {
+			escritura = new FileWriter(ruta,true);
+			escritura.write("");
+			escritura.close();
+			return true;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 	// compruebo si existe el archivo
@@ -58,6 +75,22 @@ public class Archivo {
 		}
 		
 	}
+	
+	// escribe el archivo
+	public void escribe_lineas(String frase) {
+		try
+		{
+			FileWriter entrada = new FileWriter(ruta,true);
+			BufferedWriter miBuffer = new BufferedWriter(entrada);
+			miBuffer.write(frase);
+			miBuffer.close();
+			entrada.close();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public void llenarTS(TreeSet<Persona> persona) 
 		{
 			FileReader entrada;
--- TP3_GRUPO_7/src/EJERCICIO1/Principal.java
+++ TP3_GRUPO_7/src/EJERCICIO1/Principal.java
@@ -1,12 +1,55 @@
 package EJERCICIO1;
 
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		Ejercicio ej = new Ejercicio();
+		Archivo archivo = new Archivo();
+		archivo.setRuta("Archivos\\Personas.txt");
 		
-		ej.resolverC();
+		TreeSet<Persona> listaPersonas = new TreeSet<Persona>(); // llena la lista de personas con el archivo Personas.txt
+		try
+		{
+			archivo.llenarTS(listaPersonas);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			
+		}
+		finally
+		{
+			Iterator<Persona> it = listaPersonas.iterator();
+			while(it.hasNext())
+			{
+				Persona per = (Persona)it.next();
+				System.out.println(per.toString());
+			}
+		}
+		
+		
+		
+		Archivo archivo2 = new Archivo(); // crea archivo Resultado.txt y le escribe las lineas cargadas en la listaPersonas
+		archivo2.setRuta("Archivos\\Resultado.txt");
+		
+		if(archivo2.existe())
+		{
+			System.out.println("Existe archivo");
+		}
+		else 
+		{
+			archivo2.crearArchivo();
+		}
+		
+		Iterator<Persona> it = listaPersonas.iterator();
+		while(it.hasNext())
+		{
+			Persona per = (Persona)it.next();
+			archivo2.escribe_lineas(per.toString() + "\r\n");
+		}
 
 	}
 
